fd0a2c298bf2a44e0753fc07a14d4dbdbdf890c1
livinghuang <living.huang@gmail.com>
2025-02-24 11:26:39 +0800

    revise name

--- install-chirpstack-mqtt-forwarder-as-gateway-mesh-border.sh
+++ install-chirpstack-mqtt-forwarder-as-gateway-mesh-border.sh
@@ -24,7 +24,7 @@ start_service() {
     logger -t "chirpstack-mqtt-forwarder" "Starting ChirpStack MQTT forwarder service..."
 
     procd_open_instance
-    procd_set_param command /opt/awesome_linxdot/run_chirpstack_mqtt_forwarder_as_gateway_mesh_boarder.sh
+    procd_set_param command /opt/awesome_linxdot/run_chirpstack_mqtt_forwarder_as_gateway_mesh_border.sh
     procd_set_param respawn 3600 5 0  # respawn after 3600s (1h) if fails, with 5s delay between retries
     procd_set_param stdout 1          # redirect stdout to syslog
     procd_set_param stderr 1          # redirect stderr to syslog
