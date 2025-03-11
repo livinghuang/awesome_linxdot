afcb6407fb2772f5da283949f9d3d6e8341a6f68
livinghuang <living.huang@gmail.com>
2025-03-11 10:25:15 +0800

    revise run mesh as deamon

--- run_mesh_time_sync.sh
+++ run_mesh_time_sync.sh
@@ -5,11 +5,14 @@ base_dir="/opt/awesome_linxdot/awesome-software/mesh_relay_time_sync"
 executable="$base_dir/mesh_relay_time_sync"
 log_file="/tmp/mesh_relay_time_sync.log"
 
-# Change to base directory
-cd "$base_dir" || exit 1
+while true; do
+    # Change to base directory
+    cd "$base_dir" || exit 1
 
-# Start process in the background and log output
-"$executable" > "$log_file" 2>&1 &
+    # Start process in the background and log output
+    "$executable" > "$log_file" 2>&1 &
+    sleep 300
+done
 
 # Log message to system logger
 logger -t "mesh_relay_time_sync" "Process started. Logs: $log_file"
