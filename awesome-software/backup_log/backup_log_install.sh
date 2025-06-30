f37d091382c1c7af1d4c349df71ca9317ffa6a85
livinghuang <living.huang@gmail.com>
2025-07-01 04:45:50 +0800

    add clean backup_etc

--- awesome-software/backup_log/backup_log_install.sh
+++ awesome-software/backup_log/backup_log_install.sh
@@ -79,6 +79,7 @@ install_script cleanup_old_backup \
   "KEEP_DAYS=${KEEP_DAYS:-7}" \
   'find /root              -name "backup_*.tar.gz"      -type f -mtime +"$KEEP_DAYS" -exec rm -f {} \;' \
   'find /root/backup       -name "messages_*.log"       -type f -mtime +"$KEEP_DAYS" -exec rm -f {} \;' \
+  'find /root/backup       -name "etc_*"                -type f -mtime +"$KEEP_DAYS" -exec rm -f {} \;' \
   'find /root/docker_log_backup -name "docker_logs_*.tar.gz" -type f -mtime +"$KEEP_DAYS" -exec rm -f {} \;'
 
 # system_health_check.sh
