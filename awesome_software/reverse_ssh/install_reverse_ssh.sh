a7488e2bff54880825c7593e056959b419012b9b
livinghuang <living.huang@gmail.com>
2025-07-08 08:05:30 +0800

    revise initial awesome linxdot

--- awesome_software/reverse_ssh/install_reverse_ssh.sh
+++ awesome_software/reverse_ssh/install_reverse_ssh.sh
@@ -7,15 +7,15 @@ SCRIPT_TARGET="$INSTALL_DIR/reverse_ssh.sh"
 
 # === 複製 init.d 腳本 ===
 echo "[*] 複製 init.d 腳本至 $INITD_TARGET"
-cp reverse_ssh.initd "$INITD_TARGET"
+cp "$INSTALL_DIR/reverse_ssh.initd" "$INITD_TARGET"
 chmod +x "$INITD_TARGET"
 
 # === 設為開機自啟 ===
 echo "[*] 啟用開機自動啟動"
-$INITD_TARGET enable
+"$INITD_TARGET" enable
 
 # === 立即啟動服務 ===
 echo "[*] 啟動 reverse_ssh 服務"
-$INITD_TARGET start
+"$INITD_TARGET" start
 
 echo "[✔] Reverse SSH 安裝完成並啟動成功"
