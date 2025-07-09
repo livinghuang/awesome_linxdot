79614b493705686826308b57cbe02ca2e09ccbcc
livinghuang <living.huang@gmail.com>
2025-07-10 07:11:02 +0800

    update luci

--- awesome_software/chirpstack_concentratord/luci/controller/chirpstack.lua
+++ awesome_software/chirpstack_concentratord/luci/controller/chirpstack.lua
@@ -1,7 +1,8 @@
 module("luci.controller.chirpstack", package.seeall)
 
 function index()
-    entry({"admin", "lorawan"}, template("chirpstack/status"), _("LoRaWAN"), 10)
+    entry({"admin", "lorawan"}, template("chirpstack/status"), _("LoRaWAN"), 10).leaf = true
+    entry({"admin", "lorawan", "refresh_gateway_id"}, call("action_refresh_gateway_id"), nil).leaf = true
 end
 
 function get_gateway_id()
@@ -9,8 +10,12 @@ function get_gateway_id()
     if file then
         local id = file:read("*l")
         file:close()
-        return id or "N/A"
-    else
-        return "N/A"
+        return id
     end
+end
+
+function action_refresh_gateway_id()
+    local cmd = "/opt/awesome_linxdot/awesome_software/chirpstack_concentratord/get_gateway_id.sh"
+    os.execute(cmd .. " &")
+    luci.http.redirect(luci.dispatcher.build_url("admin", "lorawan"))
 end
