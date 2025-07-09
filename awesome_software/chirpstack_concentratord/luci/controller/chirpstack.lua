d2dd56dbf6986554aa553e58d9d9acc3f809f6d0
livinghuang <living.huang@gmail.com>
2025-07-10 05:29:00 +0800

    update luci

--- awesome_software/chirpstack_concentratord/luci/controller/chirpstack.lua
+++ awesome_software/chirpstack_concentratord/luci/controller/chirpstack.lua
@@ -1,5 +1,16 @@
 module("luci.controller.chirpstack", package.seeall)
 
 function index()
-    entry({"admin", "status", "chirpstack"}, template("chirpstack/status"), _("ChirpStack Concentratord"), 90)
+    entry({"admin", "services", "chirpstack"}, template("chirpstack/status"), _("ChirpStack Concentratord"), 10)
+end
+
+function get_gateway_id()
+    local file = io.open("/tmp/gateway_id", "r")
+    if file then
+        local id = file:read("*l")
+        file:close()
+        return id or "N/A"
+    else
+        return "N/A"
+    end
 end
--- luci/controller/awesome_linxdot.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.awesome_linxdot", package.seeall)
-
-function index()
-    entry({"admin", "status", "awesome_linxdot"}, call("action_message"), _("Awesome Linxdot"), 1).dependent = false
-end
-
-function action_message()
-    luci.template.render("awesome_linxdot")
-end
