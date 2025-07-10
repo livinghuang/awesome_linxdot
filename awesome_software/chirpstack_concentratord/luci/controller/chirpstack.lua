bc87ffd0893552e1d486f67f260895e91352a142
livinghuang <living.huang@gmail.com>
2025-07-10 08:28:41 +0800

    update luci

--- awesome_software/chirpstack_concentratord/luci/controller/chirpstack.lua
+++ awesome_software/chirpstack_concentratord/luci/controller/chirpstack.lua
@@ -1,7 +1,5 @@
 module("luci.controller.chirpstack", package.seeall)
 
 function index()
-    -- 放在 root 底下（= 顯示在側邊欄第一層）
-    -- 排序數字設為 15，比概覽（20）略前
     entry({"admin", "chirpstack"}, template("chirpstack/status"), _("LoRaWAN"), 15).leaf = true
 end
